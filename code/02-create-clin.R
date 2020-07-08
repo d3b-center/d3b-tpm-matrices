3aef653d9d95888f17488a690e5a3abd117deced
komalsrathi <komal.rathi@gmail.com>
2020-07-08 14:02:23 -0400

    take existing clin file as param

--- code/02-create-clin.R
+++ code/02-create-clin.R
@@ -4,6 +4,8 @@
 # load libraries
 suppressPackageStartupMessages(library(optparse))
 suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(dplyr))
+
 
 # root directory
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
@@ -12,18 +14,36 @@ outdir <- file.path(root_dir, "data/")
 # parameters
 option_list <- list(
   make_option(c("--mat"), type = "character",
-              help = "Expression Matrix (RSEM TPM) (.RDS)"),
-  make_option(c("--library"), type = "character",
-              help = "Library Preparation Method"),
+              help = "Expression Matrix (RSEM TPM) (.rds)"),
+  make_option(c("--clin"), type = "character",
+              default = NA,
+              help = "Existing clinical file (.rds)"),
+  make_option(c("--id_col"), type = "character",
+              default = NA,
+              help = "Sample identifier column to be used (only use with --clin option)"),
+  make_option(c("--lib_col"), type = "character",
+              default = NA,
+              help = "Library column to be used (only use with --clin option)"),
+  make_option(c("--study_col"), type = "character",
+              default = NA,
+              help = "Study column to be used (only use with --clin option)"),
+  make_option(c("--lib"), type = "character",
+              default = NA,
+              help = "Library Prep Method"),
   make_option(c("--study"), type = "character",
+              default = NA,
               help = "Study identifier"),
   make_option(c("--outfile"), type = "character",
-              help = "Output filename (.RDS)"))
+              help = "Output filename (.rds)"))
 
 # parse parameters
 opt <- parse_args(OptionParser(option_list = option_list))
 mat <- opt$mat
-library <- opt$library
+clin <- opt$clin
+id_col <- opt$id_col
+lib_col <- opt$lib_col
+study_col <- opt$study_col
+library_prep <- opt$lib
 study <- opt$study
 outfile <- opt$outfile
 outfile <- file.path(outdir, outfile)
@@ -31,12 +51,40 @@ outfile <- file.path(outdir, outfile)
 # read data
 mat <- readRDS(mat)
 
-# create clinical file with input
-clin <- data.frame(sample_id = colnames(mat), study_id = study, library = library)
-clin <- clin %>%
-  mutate(tmp = sample_id,
-         batch = paste0(study, '_', library)) %>%
-  column_to_rownames('tmp')
+# some qc
+if(!is.na(clin) & !is.na(id_col) & !is.na(lib_col) & !is.na(study_col) & is.na(library_prep) & is.na(study)){
+  print("Using existing clinical file")
+  dummy <- 'clin'
+  clin <- readRDS(clin)
+} else if(is.na(clin) & !is.na(id_col) & is.na(lib_col) & is.na(study_col) & !is.na(library_prep) & !is.na(study)){
+  print("Using library and study info")
+  dummy <- 'denovo'
+} else {
+  print("Please check input parameters")
+  break
+}
+
+# create clinical file based on input parameters
+if(dummy == 'denovo'){
+  clin <- data.frame(sample_id = colnames(mat), study_id = study, library = library_prep)
+  clin <- clin %>%
+    mutate(tmp = sample_id,
+           batch = paste0(study_id, '_', library)) %>%
+    column_to_rownames('tmp')
+}
+
+# create clinical file using existing clinical file
+if(dummy == 'clin'){
+  clin <- clin %>%
+    filter(!!as.name(id_col) %in% colnames(mat)) %>%
+    dplyr::rename('sample_id' = sym(id_col),
+                  'study_id' = sym(study_col),
+                  'library' = sym(lib_col)) %>%
+    mutate(batch = paste0(study_id, '_', library))  %>%
+    dplyr::select(sample_id, study_id, library, batch) %>% 
+    mutate(tmp = sample_id) %>%
+    column_to_rownames('tmp')
+}
 
 # save output
 saveRDS(clin, file = outfile)
